c9cfa3633807f0a0167efc125e1df0cc0ae3cb04
이진우 <leejinwoo1126@gmail.com>
2025-07-30 19:28:35 +0900

    refactor: convert input url to embedded url

--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -10,12 +10,23 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
                 <h3 class="video__title"></h3>
             </section>`);
 
-        console.log(url);
-
         const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        iframe.src = 'https://www.youtube.com/embed/eti78d9CL4I'; // url -> videoId
+        iframe.src = this.convertToEmbeddedURL(url);
 
         const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
         titleElement.textContent = title;
+    }
+
+    private convertToEmbeddedURL(url: string): string {
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const match = url.match(regExp);
+
+        const videoId = match ? match[1] || match[2] : undefined;
+
+        if(videoId) {
+            return `https://www.youtube.com/embed/${videoId}`;
+        }
+
+        return url;
     }
 }
