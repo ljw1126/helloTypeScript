b299481102b9efc057030bb19c714c9426e8b018
leejinwoo <leejinwoo1126@gmail.com>
2025-07-31 15:19:20 +0900

    refactor: media, text input

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -8,81 +8,63 @@ import {InputDialog} from "./components/dialog/dialog.js";
 import {MediaSectionInput} from "./components/dialog/input/media-input.js";
 import {TextSectionInput} from "./components/dialog/input/text-input.js";
 
+export interface MediaData {
+    readonly title: string;
+    readonly url: string;
+}
+
+export interface TextData {
+    readonly title: string;
+    readonly body: string;
+}
+
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+    new () : T;
+}
+
 class App {
     private readonly page: Component & Composable;
-    constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
+    constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
         this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
 
-        const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
-        imageBtn.addEventListener('click', () => {
+        this.bindElementToDialog<MediaSectionInput>('#new-image',
+            MediaSectionInput,
+            (input: MediaSectionInput) => new ImageComponent(input.title, input.url));
+
+        this.bindElementToDialog<MediaSectionInput>('#new-video',
+            MediaSectionInput,
+            (input: MediaSectionInput) => new VideoComponent(input.title, input.url))
+
+        this.bindElementToDialog<TextSectionInput>('#new-note',
+            TextSectionInput,
+            (input: TextSectionInput) => new NoteComponent(input.title, input.body));
+
+        this.bindElementToDialog<TextSectionInput>('#new-todo',
+            TextSectionInput,
+            (input: TextSectionInput) => new TodoComponent(input.title, input.body));
+    }
+
+    private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+        selector: string,
+        InputComponent: InputComponentConstructor<T>,
+        makeSection: (input: T) => Component
+    ) {
+        const element = document.querySelector(selector)! as HTMLButtonElement;
+        element.addEventListener('click', () => {
             const dialog = new InputDialog();
-            const mediaSection = new MediaSectionInput();
-            dialog.addChild(mediaSection);
-            dialog.attachTo(dialogRoot);
+            const input = new InputComponent();
+            dialog.addChild(input);
+            dialog.attachTo(this.dialogRoot);
 
             dialog.setOnCloseListener(() => {
-                dialog.removeFrom(dialogRoot);
+                dialog.removeFrom(this.dialogRoot);
             });
 
             dialog.setOnSubmitListener(() => {
-                const image = new ImageComponent(mediaSection.title, mediaSection.url);
-                this.page.addChild(image);
-                dialog.removeFrom(dialogRoot);
-            });
-        })
-
-        const videoBtn = document.querySelector('#new-video')! as HTMLButtonElement;
-        videoBtn.addEventListener('click', () => {
-            const dialog = new InputDialog();
-            const mediaSection = new MediaSectionInput();
-            dialog.addChild(mediaSection);
-            dialog.attachTo(dialogRoot);
-
-            dialog.setOnCloseListener(() => {
-                dialog.removeFrom(dialogRoot);
-            });
-
-            dialog.setOnSubmitListener(() => {
-                const image = new VideoComponent(mediaSection.title, mediaSection.url);
-                this.page.addChild(image);
-                dialog.removeFrom(dialogRoot);
-            });
-        })
-
-        const noteBtn = document.querySelector('#new-note')! as HTMLButtonElement;
-        noteBtn.addEventListener('click', () => {
-            const dialog = new InputDialog();
-            const mediaSection = new TextSectionInput();
-            dialog.addChild(mediaSection);
-            dialog.attachTo(dialogRoot);
-
-            dialog.setOnCloseListener(() => {
-                dialog.removeFrom(dialogRoot);
-            });
-
-            dialog.setOnSubmitListener(() => {
-                const image = new NoteComponent(mediaSection.title, mediaSection.body);
-                this.page.addChild(image);
-                dialog.removeFrom(dialogRoot);
-            });
-        })
-
-        const todoBtn = document.querySelector('#new-todo')! as HTMLButtonElement;
-        todoBtn.addEventListener('click', () => {
-            const dialog = new InputDialog();
-            const mediaSection = new TextSectionInput();
-            dialog.addChild(mediaSection);
-            dialog.attachTo(dialogRoot);
-
-            dialog.setOnCloseListener(() => {
-                dialog.removeFrom(dialogRoot);
-            });
-
-            dialog.setOnSubmitListener(() => {
-                const image = new TodoComponent(mediaSection.title, mediaSection.body);
-                this.page.addChild(image);
-                dialog.removeFrom(dialogRoot);
+                const section = makeSection(input);
+                this.page.addChild(section);
+                dialog.removeFrom(this.dialogRoot);
             });
         })
     }
--- motion/src/components/dialog/input/text-input.ts
+++ motion/src/components/dialog/input/text-input.ts
@@ -1,6 +1,7 @@
 import {BaseComponent} from "../../component.js";
+import {TextData} from "../../../app.js";
 
-export class TextSectionInput extends BaseComponent<HTMLElement> {
+export class TextSectionInput extends BaseComponent<HTMLElement> implements TextData{
     constructor() {
         super(`<div>
                 <div class="form__container">
